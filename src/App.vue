77b35717b13aa9f5713be76c2fb5f192a467cedb
MioButton <you@example.com>
2020-03-28 11:06:22 +0800

    TEST4

--- src/App.vue
+++ src/App.vue
@@ -53,6 +53,10 @@
 @import "../node_modules/bootstrap/dist/css/bootstrap.css";
 body{
     padding-top: 70px;
+    background:url("../resources/bg.jpg") no-repeat;
+    background-size:cover;
+    width:10rem;
+    height:5rem;
 }
 .main-content{
     min-height: 100vh;
