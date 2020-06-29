5b8dc1b579636d6284a3bfe00ca5c8c0bda2a4ed
Haohua Lyu <haohua@sas.upenn.edu>
2020-06-30 00:28:36 +0800

    Update en-US.js

--- src/locales/en-US.js
+++ src/locales/en-US.js
@@ -5,35 +5,36 @@ const en_US = {
         author: "Mio Button Team 2020",
         null: "Null",
         subscriber: "Current YouTube Subscribers:",
-        audioStaff: "鸣谢名单以及网站相关Acknowledgement and",
-        toGithub: "请在GitHub提出您的宝贵意见",
-        notOfficial: "本站为爱好者作品，和hololive官方没有关联。上传的音频文件若有侵犯您的权益，请立即联系我们删除:samuimomijiyuki@qq.com",
-        overlapTips: "叠加播放无法暂停，同时会创建大量线程，建议偶尔刷新页面"
+        audioStaff: "Acknowledgement and Website Info",
+        toGithub: "Contact us on GitHub",
+        notOfficial: "This is a fan-made website and is not related to Hololive/Cover Corp. Please notify us for removal if you find any uploaded voice lines violate your copyrights: samuimomijiyuki@qq.com",
+        overlapTips: "Voice lines cannot be paused and lots of threads will be created when playing in Overlap Mode. Please refresh the page occasionally."
     },
     action: {
-        toggleNavbar: "切换导航栏",
-        random: "● 今日随机",
-        share: "分享到Twitter查看",
-        placeholder: "请输入昵称",
-        live: "● 信息面板",
-        vtbbtn: "VTuber按钮合集",
-        close: "关闭",
-        copy: "复制",
-        stopvoice: "停止",
-        randomplay: "随机选择",
-        overlap: "重叠播放",
-        autoplay: "自动播放",
-        playing: "正在播放：",
+        toggleNavbar: "Toggle the Navigation Bar",
+        random: "● Today's random line",
+        share: "Share to Twitter",
+        placeholder: "Please enter a name",
+        live: "● Info Panel",
+        vtbbtn: "VTuber Button Catalog",
+        close: "Close",
+        copy: "Copy",
+        stopvoice: "Stop Playing",
+        randomplay: "Random Playing",
+        overlap: "Overlap Mode",
+        autoplay: "Auto Play",
+        playing: "Now Playing: ",
         noplay: "",
-        adtitle: "● 联动项目",
-        weekly: "澪雀之森的神社周报",
-        sticker: "狼贴图",
-        plan: "计划日期",
-        ing: "正在直播"
+        adtitle: "● Collaborated Projects",
+        weekly: "Fan-made Translations and Illustrations Weekly (In Chinese)",
+        sticker: "Mio Sticker",
+        plan: "Planned Date",
+        ing: "Now Streaming"
     },
     lang: {
         'zh-CN': "简体中文",
-        'ja-JP': "日本語"
+        'ja-JP': "日本語",
+        'en-US': "English"
     }
 }
 export default en_US;
